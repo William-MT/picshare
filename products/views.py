60d0f469ac050544efa86247a7ab53c371d8bf76
William-MT <122859361+William-MT@users.noreply.github.com>
2023-01-25 06:04:45 -1200

    Add files via upload
    first addition

---
+++ products/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(req):
+    return HttpResponse("hello w")
+
+
+def fnew(req):
+    return HttpResponse("news")
+
+
